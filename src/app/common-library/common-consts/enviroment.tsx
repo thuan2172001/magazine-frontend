26646bef6e71016b373345e96fff3c027731d680
thuan2172001@gmail.com <thuan2172001@gmail.com>
2021-05-06 15:07:58 +0700

    Fix api base url in environment.tsx

--- src/app/common-library/common-consts/enviroment.tsx
+++ src/app/common-library/common-consts/enviroment.tsx
@@ -11,23 +11,7 @@ export const REACT_APP_ENV = process.env.REACT_APP_ENV;
 export const CERTIFICATE_EXP = 1000 * 60 * 60;
 export const PROJECT_NAME = 'vncheck';
 // export const APP_TITLE: string = process.env.REACT_APP_TITLE ?? '';
-export const API_BASE_URL = (() => {
-  if (REACT_APP_ENV === 'mock' || REACT_APP_ENV === 'production') return '/api';
-  if (process.env.REACT_APP_API_BASE_URL) {
-    if (process.env.REACT_APP_API_BASE_URL.charAt(0) === ':') {
-      return (
-        window.location.protocol +
-        '//' +
-        window.location.hostname +
-        process.env.REACT_APP_API_BASE_URL
-      );
-    }
-    
-    return process.env.REACT_APP_API_BASE_URL;
-  }
-  
-  return '/api';
-})();
+export const API_BASE_URL = 'https://magazine-manager-api.herokuapp.com/api'
 export const BASE_URL = API_BASE_URL.indexOf('http') === 0 ? API_BASE_URL.split('/').slice(0, 3).join('/') : '';
 
 //TODO: Pincode....
